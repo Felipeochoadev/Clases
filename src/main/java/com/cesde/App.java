1e02798c6fe78028ca881fe3522e5a330b80452d
FelipeOchoa <luisfelipeochoac@gmail.com>
2026-03-18 20:40:51 -0500

    Creacion de mensaje de salida System.out.println(), el tipo de escritura es camelCase

--- src/main/java/com/cesde/App.java
+++ src/main/java/com/cesde/App.java
@@ -6,5 +6,8 @@ public class App {
         String codigo, seccion;
         Float valorUnitario, valorBruto, descuento, iva, valorNeto;
         int cantVendida;
+
+        //Inicio del algoritmo, Leer variables
+        System.out.println("Ingrese el -> Codigo ");
     }
 }
