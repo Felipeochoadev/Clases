eff96b0d6754cfd136f8f237cd0e3c59c36a332a
FelipeOchoa <luisfelipeochoac@gmail.com>
2026-03-18 20:24:29 -0500

    Creacion de primeras variables

--- src/main/java/com/cesde/App.java
+++ src/main/java/com/cesde/App.java
@@ -2,6 +2,9 @@ package com.cesde;
 
 public class App {
     public static void main(String[] args) {
-        
+        String codigo;
+        Float valorUnitario;
+        int cantVendida;
+        String seccion;
     }
 }
