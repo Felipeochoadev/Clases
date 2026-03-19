d56b9e8817f8d765be73c5d64b58b392ba256f6b
FelipeOchoa <luisfelipeochoac@gmail.com>
2026-03-18 20:59:28 -0500

    Instanciacion del objeto escaner y declaracion inicial

--- src/main/java/com/cesde/App.java
+++ src/main/java/com/cesde/App.java
@@ -1,7 +1,11 @@
 package com.cesde;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //Instanciar la libreria de Scanner (Se usa para capturar los input)
+        Scanner scanner = new Scanner(System.in);
+
         //Definicion de variables principales
         String codigo, seccion;
         Float valorUnitario, valorBruto, descuento, iva, valorNeto;
@@ -9,5 +13,7 @@ public class App {
 
         //Inicio del algoritmo, Leer variables
         System.out.println("Ingrese el -> Codigo ");
+
+        
     }
 }
